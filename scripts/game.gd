41c9f9c3d68e14ca6dc9a7ac44c678002e2ba718
bipe <itsmemarconi@gmail.com>
2025-08-02 19:51:01 -0300

    feat: add first random events

--- scripts/game.gd
+++ scripts/game.gd
@@ -6,12 +6,14 @@ class_name Game
 var ellapsed_seconds := 0
 
 @onready var sisyphus: Node2D = $Sisyphus
-@onready var bolder: Node2D = $Bolder
+@onready var bolder: Bolder = $Bolder
 @onready var mountain: Mountain = $Mountain
 @onready var average_label: Label = %"Average Label"
 @onready var time_label: Label = %"Time Label"
 
 func _ready() -> void:
+	_generate_rand_event()
+	
 	_update_average_label()
 	PushCounter.cycle_ended.connect(_update_average_label)
 	_start_time_tracking()
@@ -36,3 +38,37 @@ func _update_average_label():
 func _add_second():
 	ellapsed_seconds += 1
 	time_label.text = "%ss" % ellapsed_seconds
+	
+func _start_rain():
+	print("it's raining")
+	bolder.mass = 3.5
+	await get_tree().create_timer(randf_range(2, 3.75)).timeout
+	bolder.mass = 1
+	
+func _receive_energy():
+	if randf() > 0.5:
+		print("real redbull")
+		sisyphus.strength = 60
+		await get_tree().create_timer(4).timeout
+	else:
+		print("fake redbull")
+		# show failed msg?
+		
+func _set_bird():
+	print("heavy ahh bird")
+	bolder.mass = 5
+	for i in 5:
+		await PushCounter.push_added
+	print("bird flew")
+	bolder.mass = 1
+
+func _thought():
+	print("sisyphus has a thought")
+	
+func _generate_rand_event():
+	# remove later:
+	await get_tree().create_timer(3).timeout
+	
+	var events = [_start_rain, _receive_energy, _set_bird, _thought] #_cerberus, _persephone, _orpheus, _sleep, _reach_top]
+	await events.pick_random().call()
+	_generate_rand_event()
